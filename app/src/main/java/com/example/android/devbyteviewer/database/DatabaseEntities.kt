c5bb216c49bbd8643fe50b3d895b2f869ad131bd
Daniel Ševčík <d_sevcik@utb.cz>
2020-12-10 12:42:26 +0100

    Added the video repository. Fixed a compilation error.

--- app/src/main/java/com/example/android/devbyteviewer/database/DatabaseEntities.kt
+++ app/src/main/java/com/example/android/devbyteviewer/database/DatabaseEntities.kt
@@ -16,6 +16,8 @@
 
 package com.example.android.devbyteviewer.database
 
+import androidx.room.Entity
+import androidx.room.PrimaryKey
 import com.example.android.devbyteviewer.domain.DevByteVideo
 
 
